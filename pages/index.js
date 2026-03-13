349a3630d043c5dca46bae60c68778cd9f4c7e8e
samiksha satarkar <samikshasatarkar08@gmail.com>
2026-03-14 02:07:10 +0530

    fix dashboard routing

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
       <iframe
         src="/index.html"
-        style={{ width: '100%', height: '100vh', border: 'none' }}
+        style={{ width: '100%', height: '100%', border: 'none' }}
       />
       {/* Floating dashboard button */}
       <Link href="/dashboard" style={{
